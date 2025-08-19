63a9a5f575191f8f47b6031e6786c0733f055769
AnnaOlenka <calderonleivaa@gmail.com>
2025-08-19 10:13:18 -0500

    .

--- src/components/Modals/CompanyPermissionsModal.tsx
+++ src/components/Modals/CompanyPermissionsModal.tsx
@@ -1207,13 +1207,21 @@ const AddUserPopover: React.FC<AddUserPopoverProps> = ({
         {/* Search */}
         <div className="p-4 pb-3 space-y-3">
           <div className="relative">
-            <Search className="w-4 h-4 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+            <Search 
+              className="w-4 h-4 text-gray-400" 
+              style={{
+                position: 'absolute',
+                left: '0.75rem',
+                top: '50%',
+                transform: 'translateY(-50%)'
+              }}
+            />
             <input
               type="text"
               placeholder="Buscar usuario..."
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className="w-full pl-9 pr-4 py-2 border border-gray-300 rounded-lg text-sm focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              className="permissions-modal-search-input"
             />
           </div>
           
